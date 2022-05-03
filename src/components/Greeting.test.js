f5a0c7a7bd51c3faf249e6c0dbbfc437d050b736
himoura32 <shawn.ahearn@outlook.com>
2022-05-02 20:52:10 -0400

    coded group, interaction, state, component/mocks

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -15,4 +16,46 @@ describe("Greeting component", () => {
     });
     expect(helloWorldElement).toBeInTheDocument();
   });
+
+  test("renders good to see you! ...if the button was not clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // ...nothing
+
+    // Assert
+    const goodToSeeYouElement = screen.getByText("good to see you!", {
+      exact: false,
+    });
+    expect(goodToSeeYouElement).toBeInTheDocument();
+  });
+
+  test("renders Changed! ...if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render good to see you ...if button clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("good to see you!", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
+  })
 });
